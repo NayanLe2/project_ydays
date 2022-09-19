1b940625799a4857aff92ae81b32574146af1eb0
Nayan Mallet <nayan.mallet@ynov.com>
2022-09-19 17:29:24 +0200

    nav bar added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 
 <template>
-  <header_bloc></header_bloc>
+    <header_bloc></header_bloc>
+    <nav_bar></nav_bar>
 </template>
 
 <script>
 import header_bloc from './components/header_bloc.vue'
+import nav_bar from './components/nav_bar.vue'
+
 
 export default {
   name: 'App',
   components: {
-      header_bloc
+      header_bloc,
+      nav_bar
   }
 }
 </script>
---
+++ src/components/nav_bar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="nav_bar">
+        <p>test</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "nav_bar"
+}
+</script>
+
+<style scoped>
+
+</style>
