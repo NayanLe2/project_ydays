4f67d00a1d61ff313053688f0b647d1d7480afb0
Nayan Mallet <nayan.mallet@ynov.com>
2022-09-19 14:46:27 +0200

    paramétrage repo (styles) + header_bloc start

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <header_bloc></header_bloc>
+</template>
+
+<script>
+import header_bloc from './components/header_bloc.vue'
+
+export default {
+  name: 'App',
+  components: {
+      header_bloc
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header_bloc.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header_bloc">
+        <h1 class="header_title">ShopNextClothes</h1>
+        <p class="header_text"></p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'header_bloc'
+}
+</script>
+
+<style scoped></style>
