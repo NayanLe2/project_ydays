900a233eb1e4b77b8a1bec262b40cce5ce51984a
Nayan Mallet <nayan.mallet@ynov.com>
2022-09-20 09:21:00 +0200

    nab bar buttons added + responsive nav bar

--- src/components/nav_bar.vue
+++ src/components/nav_bar.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="nav_bar">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="arrow-small-left">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+        </svg>
         <p>test</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="arrow-small-right">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+        </svg>
     </div>
 </template>
 
